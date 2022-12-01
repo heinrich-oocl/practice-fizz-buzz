9b14f44b49fe247a451451d055aa5449ee7853cf
Hezz <heinrich@connect.hku.hk>
2022-12-01 15:01:48 +0800

    feat: Implemented case for countOff for multiple of 3 & 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,8 +3,9 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public String countOff(int i){
-        return  i%7 == 0 ? "Whizz" :
-                i%3 == 0 ? "Fizz" :
+        return  i%15 == 0 ? "FizzBuzz":
+                i%7 == 0 ? "Whizz":
+                i%3 == 0 ? "Fizz":
                 i%5 == 0 ? "Buzz":
                 String.valueOf(i);
     }
