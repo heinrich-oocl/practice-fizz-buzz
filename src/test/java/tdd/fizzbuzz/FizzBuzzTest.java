25cdc04437bc979bd18dfdbeb95cbaeb2690996a
Hezz <heinrich@connect.hku.hk>
2022-12-01 14:49:43 +0800

    test: written test case for should_get_number_when_countOff_given_one

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,22 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    void should_get_number_when_countOff_given_one() {
+        //given
+        int sequence =1 ;
+        
+        //when
+        String result = new FizzBuzz().countOff(sequence);
+        
+        //then
+        assertEquals("1", result);
+    }
+
+
 }
